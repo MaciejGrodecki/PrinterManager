5e2f75ea76afb712c72cf74e4d8bd0a69dc58955
MaciejGrodecki <maciekg1989@gmail.com>
2018-07-11 13:01:26 +0200

    Added migration to create tbl_printer

---
+++ protected/migrations/m180711_104142_create_printer_table.php
@@ -0,0 +1,26 @@
+<?php
+
+class m180711_104142_create_printer_table extends CDbMigration
+{
+	public function safeUp()
+	{
+            $this->createTable('tbl_printer', array(
+                'id' => 'pk',
+                'name' => 'string NOT NULL',
+                'inventoryNumber' => 'string NOT NULL',
+                'room_id' => 'integer NOT NULL'
+            ));
+            
+            $this->addForeignKey('FK_printer_room', 
+                    'tbl_printer', 'room_id', 'tbl_room', 'id', 'CASCADE', 'CASCADE');
+	}
+        
+        
+        
+
+	public function safeDown()
+	{
+            $this->dropTable('tbl_printer');
+	}
+	
+}
